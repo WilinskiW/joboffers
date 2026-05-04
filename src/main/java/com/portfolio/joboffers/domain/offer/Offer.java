9452eb9f3d43d13cf1c82d5a0b1394414da093ad
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-05-04 12:29:16 +0200

    Add mongo annotations for Offer model

--- src/main/java/com/portfolio/joboffers/domain/offer/Offer.java
+++ src/main/java/com/portfolio/joboffers/domain/offer/Offer.java
@@ -2,15 +2,22 @@ package com.portfolio.joboffers.domain.offer;
 
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "offers")
 @Builder
 public record Offer(
         @Id
         String id,
+        @Field("company")
         String company,
+        @Field("position")
         String position,
+        @Field("salary")
         String salary,
+        @Field("url")
+        @Indexed(unique = true)
         String offerUrl) {
 }
